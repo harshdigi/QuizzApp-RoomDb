d1ddc912cd307807116282cf1d3c68940d0fb1f2
harshdigi <harshkahuja@gmail.com>
2022-06-03 20:34:04 +0530

    "Comments added"

--- app/src/main/java/com/digitaldealsolution/quizly/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/digitaldealsolution/quizly/Activities/SplashScreenActivity.java
@@ -36,6 +36,7 @@ public class SplashScreenActivity extends AppCompatActivity {
             public void run() {
                 Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         }, 4000);
     }
